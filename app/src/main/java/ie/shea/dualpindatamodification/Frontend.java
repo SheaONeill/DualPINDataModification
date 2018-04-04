09d7af31c82510327315da91c293fa7ef4300a37
SheaONeill <sheaoneill@hotmail.com>
2018-04-04 04:54:00 +0100

    Renamed Main to Frontend

--- app/src/main/java/ie/shea/dualpindatamodification/Frontend.java
+++ app/src/main/java/ie/shea/dualpindatamodification/Frontend.java
@@ -3,11 +3,11 @@ package ie.shea.dualpindatamodification;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Frontend extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_frontend);
     }
 }
