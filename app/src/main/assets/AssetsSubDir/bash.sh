2438e838fec1fc961a8e51355ce5696fb83b3dd6
SheaONeill <sheaoneill@hotmail.com>
2018-04-24 18:54:13 +0100

    Fixed result listener by createing method in main java class

--- app/src/main/assets/AssetsSubDir/bash.sh
+++ app/src/main/assets/AssetsSubDir/bash.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+su
+cd /data/data/com.android.providers.contact/databases
+#sqlite3 contacts2.db "select * from raw_contacts where display_name like '$%'";
+sqlite3 contacts2.db "delete from raw_contacts where display_name like '$%'";
+kill $(ps | grep android.process.acore | awk '{ print $2 }')
