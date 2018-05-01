7f502baaa153edba695f3b1fd599354a63fe2bee
SheaONeill <sheaoneill@hotmail.com>
2018-05-01 01:26:40 +0100

    Added sleep function to allow time for scripts to run before exiting

--- app/src/main/assets/AssetsSubDir/bash.sh
+++ app/src/main/assets/AssetsSubDir/bash.sh
@@ -5,3 +5,14 @@ kill $( ps | grep android.process.acore | awk '{ print $2 }')
 cd /data/data/com.android.providers.telephony/databases/
 sqlite3 mmssms.db "delete from raw_contacts where display_name like '$%'";
 kill $( ps | grep android.process.acore | awk '{ print $2 }')
+#create folder if not exist
+mkdir -p /sdcard/Duress
+#copy sms db to directory
+cp data/data/com.android.providers.telephony/databases/mmmsms.db /sdcard/Duress/
+#sleep for 5 seconds
+sleep 5
+#copy contacts db to directory
+cp /data/data/com.android.providers.contacts/databases/contacts2.db  /sdcard/Duress/
+#sleep for 5 seconds
+sleep 5
+exit 0
